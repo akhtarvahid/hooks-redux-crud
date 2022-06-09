037cb805ad9aaace18849528fe2fecf5f8e25c9b
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-06-09 10:49:17 +0530

    redux store configured

--- src/store.js
+++ src/store.js
@@ -4,10 +4,18 @@ import createSagaMiddleware from "redux-saga";
 import reducer from "./reducers";
 //import mySaga from "./sagas/sagas";
 
+const initialState = {};
+const sagaMiddleware = createSagaMiddleware();
+
+
 // create the saga middleware
 //const sagaMiddleware = createSagaMiddleware();
 // mount it on the Store
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(sagaMiddleware)
+);
 
 // then run the saga
 // sagaMiddleware.run(mySaga);
